d808cc94665961296529735a1b7e311577511c49
HaiTS <transachhai97@gmail.com>
2020-11-30 11:02:13 +0700

    format

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get("/", function () {
 
 Auth::routes(["verify" => true]);
 
-Route::get("/home", "HomeController@index")->name("home")->middleware("verified");
+Route::get("/home", "HomeController@index")
+    ->name("home")
+    ->middleware("verified");
