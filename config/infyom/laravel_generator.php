d6846709dbcdc354e7272d840f94108b98649ef4
HaiTS <transachhai97@gmail.com>
2020-11-30 10:40:31 +0700

    infyom Publish

--- config/infyom/laravel_generator.php
+++ config/infyom/laravel_generator.php
@@ -89,7 +89,7 @@ return [
     |
     */
 
-    'templates'         => 'adminlte-templates',
+    'templates'         => 'coreui-templates',
 
     /*
     |--------------------------------------------------------------------------
